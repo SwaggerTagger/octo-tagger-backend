a1103aacc553310cf77c3cc64a475d67583ea452
John Zielke <j.l.zielke@gmail.com>
2017-05-06 13:36:33 +0200

    Images are now sorted by upload-date, not imageId

--- app/models/daos/ImageDAOImpl.scala
+++ app/models/daos/ImageDAOImpl.scala
@@ -33,7 +33,7 @@ class ImageDAOImpl @Inject() (protected val dbConfigProvider: DatabaseConfigProv
   }
 
   override def listOwnImages(userId: UUID): Future[Seq[TaggingImage]] = {
-    val query = ImageDAOImpl.images.filter(_.ownedBy === userId).sortBy(_.imageId.asc).result
+    val query = ImageDAOImpl.images.filter(_.ownedBy === userId).sortBy(_.uploadedAt.desc).result
     db.run(query)
   }
 
